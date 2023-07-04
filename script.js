f5b4b7614af06968eed75bc3b4eb916a79b32f58
Ulises <ulises493rojas@gmail.com>
2023-07-04 15:18:38 -0600

    calse 03 completada

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ const btn = document.querySelector('[data-form-btn]');
 const cretateTask = (evento) => {
     evento.preventDefault();
     const input = document.querySelector('[data-form-input]');
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector('[data-list]');
+    const task = document.createElement('li');
+    task.classList.add('card');
+    input.value = '';
+
+    const content = `
+        <div>
+            <i class="far fa-check-square icon"></i>
+            <span class="task">${value}</span>
+        </div>
+        <i class="fas fa-trash-alt trashIcon icon"></i>
+    `
+    task.innerHTML = content;
+
+    list.appendChild(task);
+
+    console.log(value);
+    console.log(content);
 }
 
 console.log(btn);
